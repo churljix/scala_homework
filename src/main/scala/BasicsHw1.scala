afb8c70e0c6769d05dafdb333178b4b3e0191453
churljix <dzeina13@inbox.lv>
2020-09-28 19:42:07 +0300

    First homework solution

--- src/main/scala/BasicsHw1.scala
+++ src/main/scala/BasicsHw1.scala
@@ -1,3 +1,14 @@
 object BasicsHw1 extends App {
-  println("Hello, World!")
+  //https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
+  def gcd(a: Int, b: Int): Int = {
+    // when remainder is 0, the previous remainder ir the gcd
+    if (b == 0)
+      Math.abs(a)
+    else
+      gcd(b, a % b)
+  }
+
+  def lcm(a: Int, b: Int): Int = {
+    Math.abs(a * b) / gcd(a, b)
+  }
 }
