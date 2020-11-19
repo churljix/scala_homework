58e9f83a75f2abb966c052040697abe5525dd2c9
churljix <dzeina13@inbox.lv>
2020-11-19 02:07:00 +0200

    SharedStateHw15

---
+++ src/main/scala/SharedStateHw15.scala
@@ -0,0 +1,84 @@
+import cats.Monad
+import cats.effect.concurrent.Ref
+import cats.effect.{Clock, Concurrent, ExitCode, IO, IOApp, Timer}
+
+import scala.concurrent.duration._
+import cats.implicits._
+
+
+/*
+ * Please implement a Cache which allows concurrent access.
+ *
+ * Tip: checking expiration could be represented as some infinite process somewhere in background
+ *
+ * Tip: you can use following structure to get current time suspended in effect : Clock[F].realTime(MILLISECONDS).flatMap(...)
+ *
+ * Cached items should have an expiration timestamp after which they are evicted.
+ *
+ * If we will put a value with the same key then it should renew expiration
+ */
+object SharedStateHw15 extends IOApp {
+
+  trait Cache[F[_], K, V] {
+    def get(key: K): F[Option[V]]
+
+    def put(key: K, value: V): F[Unit]
+  }
+
+  class RefCache[F[_] : Clock : Monad, K, V](
+                                              state: Ref[F, Map[K, (Long, V)]],
+                                              expiresIn: FiniteDuration
+                                            ) extends Cache[F, K, V] {
+
+    def get(key: K): F[Option[V]] = state.get.map(x => x.get(key).map{case (k, v) => v})
+
+    def put(key: K, value: V): F[Unit] = {
+      Clock[F].realTime(MILLISECONDS).flatMap(currentTime => state.update(x => x ++ Map(key -> (currentTime + expiresIn.toMillis, value))))
+    }
+  }
+
+  object Cache {
+    def of[F[_] : Clock, K, V](expiresIn: FiniteDuration,
+                               checkOnExpirationsEvery: FiniteDuration
+                              )(implicit T: Timer[F], C: Concurrent[F]): F[Cache[F, K, V]] = {
+      for {
+        state <- Ref.of[F, Map[K, (Long, V)]](Map.empty)
+        _     <- C.start((for {
+                   _ <- T.sleep(checkOnExpirationsEvery)
+                   time <- Clock[F].realTime(MILLISECONDS)
+                   _ <- state.getAndUpdate(x => x.mapFilter({ case (a, b) if time > a => None; case (a, b) => Some((a, b))}))
+                 } yield ()).foreverM.void)
+      } yield new RefCache(state, expiresIn)
+    }
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+
+    for {
+      cache <- Cache.of[IO, Int, String](10.seconds, 4.seconds)
+      _ <- cache.put(1, "Hello")
+      _ <- cache.put(2, "World")
+      _ <- cache.get(1).flatMap(s => IO {
+        println(s"first key $s")
+      })
+      _ <- cache.get(2).flatMap(s => IO {
+        println(s"second key $s")
+      })
+      _ <- IO.sleep(12.seconds)
+      _ <- cache.get(1).flatMap(s => IO {
+        println(s"first key $s")
+      })
+      _ <- cache.get(2).flatMap(s => IO {
+        println(s"second key $s")
+      })
+      _ <- IO.sleep(12.seconds)
+      _ <- cache.get(1).flatMap(s => IO {
+        println(s"first key $s")
+      })
+      _ <- cache.get(2).flatMap(s => IO {
+        println(s"second key $s")
+      })
+    } yield ExitCode.Success
+  }
+}
+
